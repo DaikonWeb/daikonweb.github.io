55f78c24ea06f792a766337447ee21b479cb0389
Alessio Coser <alessio.coser@xpeppers.com>
2020-03-02 09:44:44 +0100

    check tag already exists

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,13 @@ PROJECT_DIR=$(cd $(dirname $0)/..; pwd -P)
 PROJECT_NAME=$(basename $PROJECT_DIR)
 TAG=$1
 
+cd ${PROJECT_DIR}
+
+if git rev-parse "$TAG" >/dev/null 2>&1; then
+  echo "tag $TAG already exists";
+  exit 0
+fi
+
 sed -ie "s|'com.github.DaikonWeb:${PROJECT_NAME}:.*'|'com.github.DaikonWeb:${PROJECT_NAME}:${TAG}'|g" docs/quickstart.md
 sed -ie "s|<version>.*</version>|<version>${TAG}</version>|g" docs/quickstart.md
 
